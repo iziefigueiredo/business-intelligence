17ec06c1bed7834e44a1bfea45b26272a2633921
iziefigueiredo <figueiredo_isadora@hotmail.com>
2025-09-06 00:34:30 -0300

    clean columns municipio

--- src/transform/processing_ibge.py
+++ src/transform/processing_ibge.py
@@ -16,12 +16,18 @@ def rename_select(df: pd.DataFrame) -> pd.DataFrame:
     })
      return df_final
 
+
+def clean_columns(df: pd.DataFrame, columns: str) -> pd.DataFrame:
+    df[columns] = df[columns].str.split('-').str[0].str.strip()
+    return df
+
 pasta_raw = Path("data/raw/")
 arquivo_raw = pasta_raw / "pib_municipios.csv"
 df_pib = pd.read_csv(arquivo_raw)
 
 # Renomeia as colunas
 df_pib_renomeado = rename_select(df_pib)
+df_pib_renomeado = clean_columns(df_pib_renomeado, "municipio")
 
 # Define a pasta e o caminho do arquivo processado
 pasta_interim = Path("data/interim/")
