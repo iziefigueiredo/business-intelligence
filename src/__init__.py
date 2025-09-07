71ebc282f2fcec4a60c12d41c73e9d3497e1f666
iziefigueiredo <figueiredo_isadora@hotmail.com>
2025-09-07 03:41:30 -0300

    add pipeline_ibge

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from . import extract, transform, merge
-from . import pipeline_sim
+from . import pipeline_sim, pipeline_ibge
 
 
-__all__ = ["extract", "transform", "merge", "pipeline_sim"]
+__all__ = ["extract", "transform", "merge", "pipeline_sim", "pipeline_ibge"]
 
